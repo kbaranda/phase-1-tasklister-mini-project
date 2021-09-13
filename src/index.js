b8f930014abdae202482d650468ce7427763d638
Kat <bolivarkat@yahoo.com>
2021-09-13 12:31:07 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener("submit", (e) => {
+    e.preventDefault()
+    create(e.target.querySelector("#new-task-description").value)
+  })
 });
+
+function create(enteredInput) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteTodo)
+  p.textContent = `${enteredInput}  `
+  btn.textContent = ' x'
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function deleteTodo(e) {
+  e.target.parentNode.remove()
+}
+
